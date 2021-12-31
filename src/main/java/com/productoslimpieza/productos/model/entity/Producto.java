c6bba7566db340434cf31453a72514086d494010
salvatierra1 <victordanielsalvatierra6@gmail.com>
2021-12-31 13:04:28 -0300

    Develop - atributo y entidad clase producto

--- src/main/java/com/productoslimpieza/productos/model/entity/Producto.java
+++ src/main/java/com/productoslimpieza/productos/model/entity/Producto.java
@@ -1,6 +1,27 @@
 package com.productoslimpieza.productos.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "producto")
+@Getter
+@Setter
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+
+    private String marca;
+
+    private int codigo;
+
+    private LocalDate fechaVencimiento;
 
 }
